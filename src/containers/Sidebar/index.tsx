5ac145afa93ba16d182f5312327df5ef0d415160
Manoel Paulo <paulo.neto.jp@hotmail.com>
2025-08-28 15:58:12 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
     <aside>
-        <img src="https://github.com/manoelpaulo5.png" />
-        <Titulo fontSize={20}>Manoel Paulo</Titulo>
+        <SidebarContainer>
+            <Avatar />
+            <Titulo fontSize={20}>Manoel Paulo</Titulo>
+            <Paragrafo tipo="secundario" fontSize={16}>
+                manoelpaulo5
+            </Paragrafo>
+            <Descricao tipo="principal" fontSize={12}>
+                Engenheiro front-end
+            </Descricao>
+            <BotaoTema>Trocar tema</BotaoTema>
+        </SidebarContainer>
     </aside>
 )
 
